2f3a68b81f09d84608b5b8f86dd022a03bea1a89
Joe <jparkkennaby@gmail.com>
2020-09-05 14:35:01 +0100

    setup hash table exmaples methods get and set. run examples in hashTableExample function. add big O documentation

--- src/scripts/data-structures/hash-tables.js
+++ src/scripts/data-structures/hash-tables.js
@@ -40,13 +40,93 @@ class HashTable {
         this.data = new Array(size);
     }
 
+    /**
+     * hash method:
+     * O(1)
+     */
     _hash(key) {
         let hash = 0;
         for (let i = 0; i < key.length; i++) {
-            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
         }
         return hash;
-    }
+    };
+
+    /**
+     * set method:
+     * O(1)
+     */
+    set(key, value) {
+
+        const address = this._hash(key);
+
+        // console.log('set hashed key: ', address);
+
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+
+        this.data[address].push([key, value]);
+
+        return this.data;
+
+    };
+
+    /**
+     * get method:
+     * 
+     * if no collisions: O(1)
+     * if collisions O(n)
+     */
+    get(key) {
+
+        const address = this._hash(key);
+
+        // console.log('get hashed key: ', hashedKey);
+
+        const currentBucket = this.data[address];
+
+        if (!currentBucket) {
+            return undefined;
+        }
+
+
+        let bucketItemValue;
+        currentBucket.forEach((bucketItem) => {
+
+            if (key !== bucketItem[0]) {
+                return;
+            }
+
+            bucketItemValue = bucketItem[1];
+
+        });
+
+        return bucketItemValue;
+
+    };
+
 }
 
-export default hashTables;
+function hashTableExample() {
+
+    console.log('Data Structure: Hash Tables - Start');
+
+    const hashTable = new HashTable(50);
+
+    // allocating fewer memory spaces will cause a collision making the get method O(n)
+    // const hashTable = new HashTable(2); 
+
+    hashTable.set('grapes', 10000);
+    console.log(hashTable.get('grapes'));
+
+    hashTable.set('apples', 9);
+    console.log(hashTable.get('apples'));
+
+    // console.log(hashTable.data);
+
+    console.log('Data Structure: Hash Tables - End');
+
+}
+
+export default hashTableExample;
