bb1953148e032d090a8fc6eef7a01c7db4d03b91
Joe <jparkkennaby@gmail.com>
2020-09-05 17:55:14 +0100

    setup Linkedlist class to demonstrate linked list data structure

--- src/scripts/data-structures/linkedLists.js
+++ src/scripts/data-structures/linkedLists.js
@@ -53,3 +53,30 @@ delete ObjectOne;
 // console.log('objectOne: ', objectOne);
 console.log('objectTwo: ', objectTwo);
 
+let linkedList = {
+    head: {
+        value: 10,
+        next: {
+            value: 5,
+            next: {
+                value: 16,
+                next: null
+            }
+        }
+    }
+};
+
+class LinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+}
+
+const linkedList = new LinkedList(10);
+
+console.log(linkedList);
