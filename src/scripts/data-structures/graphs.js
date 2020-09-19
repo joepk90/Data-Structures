9e60432a6a6192b9c32c55dbdced66e8b28d4874
Joe <jparkkennaby@gmail.com>
2020-09-19 22:56:06 +0100

    add graphs.js + graphs data structure documentation

---
+++ src/scripts/data-structures/graphs.js
@@ -0,0 +1,19 @@
+/**
+ * Graphs
+ *
+ * One of the most useful and used data structure  in computer science when it comes to ...modelling real life
+ *
+ * A graph a set of values that are related in a pair wise fashion
+ *
+ * Graphs contain:
+ * - Nodes (Vertexes)
+ * - Edges (connections)
+ *
+ * Graphs can be used to represent Friendship circles, family trees, networks (i.e. roads connecting cities or the wordwide web)
+ * - Amazon: Recommnedtion Engines
+ * - Facebook: Social Network
+ * - Google Maps: To determine the shortest path
+ *
+ * Directed and Undirected Graphs
+ *
+ */
